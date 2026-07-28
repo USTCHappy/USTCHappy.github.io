51a374910f2567babb8745f13a7fef5fa6eb49db
Gao Xing <ustchappy@mail.ustc.edu.cn>
2026-07-28 22:50:22 +0800

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector('.theme-toggle');
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') document.body.classList.add('dark');
+toggle?.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+});
